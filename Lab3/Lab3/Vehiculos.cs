eb82b4f3eeea43553eb317c2b0e848fcc97b191e
CamiBacan <cesepulveda@miuandes.cl>
2018-04-17 17:21:49 -0300

    Cambie los tipos de vehiculos a clase en vez de enum

--- Lab3/Lab3/Vehiculos.cs
+++ Lab3/Lab3/Vehiculos.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 
 namespace Lab3
 {
-    public enum Tipo { auto, acuatico, moto, camion, bus, maqPesada };
+    //public enum Tipo { auto, acuatico, moto, camion, bus, maqPesada };
 
     public class Vehiculos
     {
         private string patente;
-        private Tipo tipo;
+        //private Tipo tipo;
 
-        public Vehiculos(string Patente, Tipo tip)
+        public Vehiculos(string Patente/*, Tipo tip*/)
         {
            patente = Patente;
-            tipo = tip;
+            //tipo = tip;
         }
 
         public string GetPatente()
@@ -25,5 +25,63 @@ namespace Lab3
         }
     }
 
+    public class Auto : Vehiculos
+    {
+        int Precio;
 
+        public Auto(string patente, int precio) : base(patente)
+        {
+            Precio = precio;
+        }
+    }
+
+    public class Acuatico : Vehiculos
+    {
+        int Precio;
+
+        public Acuatico(string patente, int precio) : base(patente)
+        {
+            Precio = precio;
+        }
+    }
+
+    public class Moto : Vehiculos
+    {
+        int Precio;
+
+        public Moto(string patente, int precio) : base(patente)
+        {
+            Precio = precio;
+        }
+    }
+
+    public class Camion : Vehiculos
+    {
+        int Precio;
+
+        public Camion(string patente, int precio) : base(patente)
+        {
+            Precio = precio;
+        }
+    }
+
+    public class Bus : Vehiculos
+    {
+        int Precio;
+
+        public Bus(string patente, int precio) : base(patente)
+        {
+            Precio = precio;
+        }
+    }
+
+    public class MaqPesada : Vehiculos
+    {
+        int Precio;
+
+        public MaqPesada(string patente, int precio) : base(patente)
+        {
+            Precio = precio;
+        }
+    }
 }
